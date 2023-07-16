7efb84f01bd31c876e42565b7e93577e6ec7ae27
IRMuhammadjon <mirahmatullayev@gmail.com>
2023-07-16 11:27:00 +0500

    salon

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+short studentsoni = 30;
+int population = 40_000;
+long worldpopulation = 7_000_000_000;
+
+Console.Write(studentsoni);
+System.Console.WriteLine( population);
+System.Console.WriteLine( worldpopulation);
+
+// size 
+System.Console.WriteLine(sizeof(short));
+System.Console.WriteLine(sizeof(int));
+System.Console.WriteLine(sizeof(long));
+
+
+//range
+
+System.Console.WriteLine(short.MaxValue);
+System.Console.WriteLine(short.MinValue);
+
+System.Console.WriteLine(int.MaxValue);
+System.Console.WriteLine(int.MinValue);
+
+System.Console.WriteLine(long.MaxValue);
+System.Console.WriteLine(long.MinValue);
